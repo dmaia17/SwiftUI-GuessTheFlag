6c436081db4c786f2abd7fc062e790b45e27a057
danielbs2 <daniel.passos@bs2tecnologia.com.br>
2022-04-20 15:27:27 -0300

    add project

---
+++ SwiftUI-GuessTheFlag/SwiftUI-GuessTheFlag/SwiftUI_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GuessTheFlagApp.swift
+//  SwiftUI-GuessTheFlag
+//
+//  Created by Daniel Maia dos Passos on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
